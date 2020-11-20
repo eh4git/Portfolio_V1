cf56f3641875e87650c5f967e506501523a7eb10
erik hirsch <ehirsch760@gmail.com>
2020-11-20 12:13:13 -0800

    landing page css, animation, icons

--- portfolio/pages/_app.js
+++ portfolio/pages/_app.js
@@ -1,11 +1,14 @@
 import '../styles/globals.css'
+import '@fortawesome/fontawesome-free/js/fontawesome';
+import '@fortawesome/fontawesome-free/js/solid';
+import '@fortawesome/fontawesome-free/js/regular';
+import '@fortawesome/fontawesome-free/js/brands';
 import { ContextProvider } from '../context/index'
-function MyApp({ Component, pageProps }) {
+
+export default function MyApp({ Component, pageProps }) {
   return (
     <ContextProvider>
       <Component {...pageProps} />
     </ContextProvider>
   )
 }
-
-export default MyApp
--- portfolio/pages/index.js
+++ portfolio/pages/index.js
@@ -3,38 +3,69 @@ import Link from 'next/link'
 import Pdf from "../public/assets/docs/WebDevResume.pdf";
 import Docx from "../public/assets/docs/WebDevResume.docx"
 import profileText from "../profileText"
-// import '../assets/css/main.css';
+import landPage from "../styles/landingPage.module.css";
 
 function LandingPage(props) {
- const onPdfClick = ()=> {
+
+  const onPdfClick = () => {
     window.open(Pdf);
   }
-  const onDocxClick = ()=> {
+
+  const onDocxClick = () => {
     window.location.replace(Docx);
   }
-return (
-    <div className="wrapper">
-      <section className="parallax-container">
-        <div className="parallax">
-          <div className="logo">
-            <div className="line topLine"></div>
-            <div className="content">
-              <div className="vintage name">Erik Hirsch</div>
-              <div className="since">- Est. 2020 -</div>
-              <div className="desc">Full-Stack Web Developer</div>
-              <div className="skillz">HTML&nbsp;+&nbsp;CSS&nbsp;+&nbsp;JS</div>
-              <div className="links">
-                <a target="_blank" rel="noopener noreferrer" href={profileText.profiles[0].href}><i
-                  className="fa fa-linkedin-square"></i></a>
-                <a target="_blank" rel="noopener noreferrer" href={profileText.profiles[1].href}><i className="fa fa-github"></i></a>
-                <a target="_blank" rel="noopener noreferrer" onClick={onPdfClick}><i className="fa fa-file-pdf-o"></i></a>
-                <a target="_blank" rel="noopener noreferrer" onClick={onDocxClick}><i className="fa fa-file-word-o"></i></a>
-                {/* <a target="_blank" href={profileText.profiles[2].href}><i className="fa fa-codepen"></i></a> */}
+
+  return (
+    <div className={landPage.wrapper}>
+      <section className={landPage.parallaxContainer}>
+        <div className={landPage.parallax}>
+          <div className={landPage.logo}>
+            <div className={`${landPage.line} ${landPage.topLine}`}></div>
+            <div className={landPage.content}>
+              <div className={`${landPage.logo} ${landPage.vintage}`}>Erik Hirsch</div>
+              <div className={landPage.since}>- Est. 2020 -</div>
+              <div className={landPage.desc}>Full-Stack Web Developer</div>
+              <div className={landPage.skills}>HTML&nbsp;+&nbsp;CSS&nbsp;+&nbsp;JS</div>
+              <div className={landPage.links}>
+                <a
+                  className={`${landPage.linkOne} ${landPage.links}`}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href={profileText.profiles[0].href}
+                >
+                  <i className='fab fa-linkedin'></i>
+                </a>
+                <a
+                  className={`${landPage.linkTwo} ${landPage.links}`}
+                  target="_blank" rel="noopener noreferrer"
+                  href={profileText.profiles[1].href}
+                >
+                  <i className='fab fa-github'></i>
+                </a>
+                <a
+                  className={`${landPage.linkThree} ${landPage.links}`}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  onClick={onPdfClick}
+                >
+                  <i className='far fa-file-pdf'></i>
+                </a>
+                <a
+                  className={`${landPage.linkFour} ${landPage.links}`}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  onClick={onDocxClick}
+                >
+                  <i className='far fa-file-word'></i>
+                </a>
+                {/* <a target="_blank" href={profileText.profiles[2].href}><i className={`${landPage.linkOne} fa fa-codepen`}></i></a> */}
               </div>
             </div>
-            <div className="line bottomLine"></div>
-            <div id="buttonInsertSpace">
-           <Link id='viewProfileBtn' href={profileText.profileSections[0].href}>View Profile</Link>
+            <div className={`${landPage.line} ${landPage.bottomLine}`}></div>
+            <div className={landPage.buttonInsertSpace}>
+              <Link href={profileText.profileSections[0].href} >
+                <a className={landPage.viewProfileBtn} >View Profile</a>
+              </Link>
             </div>
           </div>
         </div>
@@ -43,6 +74,6 @@ return (
   )
 
 }
-  
+
 export default LandingPage;
 
