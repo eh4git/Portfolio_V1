c7a9c3e5beb1634d70e24fa5c75d3f33f4b91d93
eh4git <ehirsch760@gmail.com>
2022-05-17 09:01:27 -0700

    img paths

--- profileText.js
+++ profileText.js
@@ -55,56 +55,56 @@ const profileText =
             "description": "This application was built to provide internet users with a centralized location to look up information on movies and to be presented with movie trailers and similar movie recommendations. This project was a great exercise in utilizing multiple entertainment API's to present the user with useful information relevant to the scope of the application. Additionally, building this application helped reinforce skills with jquery, HTML, and CSS.",
             "repository": "https://github.com/eh4git/YouMDb-Movie-App",
             "deployed": "https://eh4git.github.io/YouMDb-Movie-App/",
-            "image": "/../public/assets/images/projects/youMDB.png"
+            "image": "https://github.com/eh4git/Portfolio_V1/blob/master/public/assets/images/projects/youMDB.png?raw=true"
         },
         {
             "name": "Weather Forecast Application",
             "description": "In this application I have created a weather dashboard. The dashboard contains a search box that the user will input their desired city name, when the search button is clicked the weather for the city will be displayed as well as a five day forecast below it.",
             "repository": "https://github.com/eh4git/WeatherApp",
             "deployed": "https://eh4git.github.io/WeatherApp/",
-            "image": "/../public/assets/images/projects/weatherDashBoard.PNG"
+            "image": "https://github.com/eh4git/Portfolio_V1/blob/master/public/assets/images/projects/weatherDashBoard.PNG?raw=true"
         },
         {
             "name": "Story Time Adventure",
             "description": "In this application that I created, in collaboration with fellow students, the user is able to create an account and sign in using a password that is encrypted and stored in a database. After signing in the user can add children to their account and provide information specific to each child. The user can then create a unique story that incorporates the information for the child.",
             "repository": "https://github.com/eh4git/StoryTime",
             "deployed": "https://fathomless-waters-70908.herokuapp.com/",
-            "image": "/../public/assets/images/projects/storyTime.PNG"
+            "image": "https://github.com/eh4git/Portfolio_V1/blob/master/public/assets/images/projects/storyTime.PNG?raw=true"
         },
         {
             "name": "Once Upon an Adventure",
             "description": "This full stack application incorporates MongoDB, Express, React, Node.js. The application give users the ability to create a unique story while playing a game that contains educational mini games. Save the king then challenge him to a game!",
             "repository": "https://github.com/eh4git/once-upon-an-adventure",
             "deployed": "https://www.onceuaa.com/",
-            "image": "/../public/assets/images/projects/onceUponAnAdventure.png"
+            "image": "https://github.com/eh4git/Portfolio_V1/blob/master/public/assets/images/projects/onceUponAnAdventure.png?raw=true"
         },
         {
             "name": "Along With Me",
             "description": "An application built using React Native that enhances a users experience while reading their favorite literature. Provides the ability for users to trigger sounds that are tied to key words within the text. Key words are highlighted to show users what can be pressed to trigger the corresponding sound.",
             "repository": "https://github.com/eh4git/Along-With-Me",
             "deployed": "https://play.google.com/store/apps/details?id=com.rnapp2021",
-            "image": "/../public/assets/images/projects/along-with-me.png"
+            "image": "https://github.com/eh4git/Portfolio_V1/blob/master/public/assets/images/projects/along-with-me.png?raw=true"
         },
         {
             "name": "E-Commerce Application",
             "description": "A React application that provides a basic framework for an e-commerce web application. Users can favorite and add items to a cart. Users can also manage the items in the cart and can place their order",
             "repository": "https://github.com/eh4git/Scrimba_ECommerce",
             "deployed": false,
-            "image": "/../public/assets/images/projects/scrimbaEcommerce.PNG"
+            "image": "https://github.com/eh4git/Portfolio_V1/blob/master/public/assets/images/projects/scrimbaEcommerce.PNG?raw=true"
         },
         {
             "name": "Time2Fly",
             "description": "A React Native application that allows flight attendants to easily track their hours and know when they will reach their maximum allowed work time. ",
             "repository": "Private",
             "deployed": false,
-            "image": "/../public/assets/images/projects/time2Fly.png"
+            "image": "https://github.com/eh4git/Portfolio_V1/blob/master/public/assets/images/projects/time2Fly.png?raw=true"
         },
         {
             "name": "React TypeScript Minesweeper",
             "description": "The classic game of minesweeper with a few improvements. Users always open multiple tiles on their first click and can choose four different difficulty settings. More features and UI improvements to come.",
             "repository": "https://github.com/eh4git/Scrimba_ECommerce",
             "deployed": false,
-            "image": "/../public/assets/images/projects/reactMinesweeper.PNG"
+            "image": "https://github.com/eh4git/Portfolio_V1/blob/master/public/assets/images/projects/reactMinesweeper.PNG?raw=true"
         },
 
     ],
@@ -120,7 +120,7 @@ const profileText =
 
     "landingPage": "/",
     "profilePage": "/profile",
-    "profilePicture": "/../public/assets/images/ProfilePic.jpg",
+    "profilePicture": "https://github.com/eh4git/Portfolio_V1/blob/master/public/assets/images/ProfilePic.jpg?raw=true",
     "skills": {
         language: ['HTML', 'CSS', 'JavaScript', 'PHP', 'TypeScript'],
         technical: [ 'jQuery', 'AJAX', 'Express', 'Node.js', 'React.js', 'React Native', 'Redux', 'Bootstrap', 'MaterialUI', 'Git', 'Apache'],
