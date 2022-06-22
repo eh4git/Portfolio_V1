5f19faf903ce7d419f1e91ed36fcc735803b5a9d
eh4git <ehirsch760@gmail.com>
2022-06-21 18:56:36 -0700

    relink cdn for img assets

--- profileText.js
+++ profileText.js
@@ -1,133 +1,159 @@
-const profileText =
-{
-    "aboutMe": [`My name is Erik Hirsch, I am a full stack developer who incorporates technical ability with collaborative skills to produce innovative web and mobile applications. I truly enjoy collaborating and team environments and I have a passion for learning and problem solving. A few of my technical skills include HTML, CSS, JavaScript, JQuery, ES6, Node, MySQL, MongoDB, React, React Native, and Redux. I completed the Web Development Coding Boot Camp at UCLA Extension. The course has given me a solid foundation of technical skills as well as improved my ability to adapt to and learn new ideas and concepts. I am excited to transition into a new career that will challenge me and help me to develop my skills.`,
+const profileText = {
+  aboutMe: [
+    `My name is Erik Hirsch, I am a full stack developer who incorporates technical ability with collaborative skills to produce innovative web and mobile applications. I truly enjoy collaborating and team environments and I have a passion for learning and problem solving. A few of my technical skills include HTML, CSS, JavaScript, JQuery, ES6, Node, MySQL, MongoDB, React, React Native, and Redux. I completed the Web Development Coding Boot Camp at UCLA Extension. The course has given me a solid foundation of technical skills as well as improved my ability to adapt to and learn new ideas and concepts. I am excited to transition into a new career that will challenge me and help me to develop my skills.`,
 
-        `I was born and raised in Southern California and have always admired the concept of developing. Being able to intertwine my creativity and problem-solving abilities with my passion for learning and skill development is what unanimously inspired me to choose web development as a career. This, along with my commitment, resilience, and strong work ethic that was instilled in me at a young age, inevitably became the catalyst for me to earn my certificate in full stack web development from UCLA Extension in July of 2020. Furthermore, I also hold a certificate in business management from Victor Valley College and developed in-depth knowledge on how to fabricate responsive websites and mobile applications, all of which has positioned me to become the respected web developing asset I am today at Third Project Development.`,
+    `I was born and raised in Southern California and have always admired the concept of developing. Being able to intertwine my creativity and problem-solving abilities with my passion for learning and skill development is what unanimously inspired me to choose web development as a career. This, along with my commitment, resilience, and strong work ethic that was instilled in me at a young age, inevitably became the catalyst for me to earn my certificate in full stack web development from UCLA Extension in July of 2020. Furthermore, I also hold a certificate in business management from Victor Valley College and developed in-depth knowledge on how to fabricate responsive websites and mobile applications, all of which has positioned me to become the respected web developing asset I am today at Third Project Development.`,
 
-        `There is no denying that I love my career, and I take pride in delivering top-quality, fully tailored services that support my client in reaching new levels of digital success. However, in my free time, you can find me enjoying the outdoors, animals, avidly hiking, and being a proud husband and father. In fact, my family is my biggest inspiration and motivation that pushes me to be the best that I can be.
-    `],
+    `There is no denying that I love my career, and I take pride in delivering top-quality, fully tailored services that support my client in reaching new levels of digital success. However, in my free time, you can find me enjoying the outdoors, animals, avidly hiking, and being a proud husband and father. In fact, my family is my biggest inspiration and motivation that pushes me to be the best that I can be.
+    `,
+  ],
 
-    "repositories": [
-        {
-            "name": "YouMdB Movie Application",
-            "href": "https://github.com/abazrafshan/YouMDb-Movie-App.git"
-        },
-        {
-            "name": "Weather Forecast Application",
-            "href": "https://github.com/eh4git/WeatherApp"
-        },
-        {
-            "name": "Coding Quiz Challenge Application",
-            "href": "https://github.com/eh4git/CodingQuiz"
-        }
-    ],
-
-    "profiles": [
-        {
-            "name": "LinkedIn",
-            "href": "https://www.linkedin.com/in/e-hirsch/"
-        },
-        {
-            "name": "Github",
-            "href": "https://github.com/eh4git"
-        },
-    ],
-
-    "profileSections": [
-        {
-            "name": "Profile",
-            "href": "/profile/"
-        },
-        {
-            "name": "Projects",
-            "href": "/projects/"
-
-        },
-        {
-            "name": "Contact",
-            "href": "/contact/"
-        },
-    ],
-
-    "projects": [
-        {
-            "name": "YouMdB Movie Application",
-            "description": "This application was built to provide internet users with a centralized location to look up information on movies and to be presented with movie trailers and similar movie recommendations. This project was a great exercise in utilizing multiple entertainment API's to present the user with useful information relevant to the scope of the application. Additionally, building this application helped reinforce skills with jquery, HTML, and CSS.",
-            "repository": "https://github.com/eh4git/YouMDb-Movie-App",
-            "deployed": "https://eh4git.github.io/YouMDb-Movie-App/",
-            "image": "https://github.com/eh4git/Portfolio_V1/blob/master/public/assets/images/projects/youMDB.png?raw=true"
-        },
-        {
-            "name": "Weather Forecast Application",
-            "description": "In this application I have created a weather dashboard. The dashboard contains a search box that the user will input their desired city name, when the search button is clicked the weather for the city will be displayed as well as a five day forecast below it.",
-            "repository": "https://github.com/eh4git/WeatherApp",
-            "deployed": "https://eh4git.github.io/WeatherApp/",
-            "image": "https://github.com/eh4git/Portfolio_V1/blob/master/public/assets/images/projects/weatherDashBoard.PNG?raw=true"
-        },
-        {
-            "name": "Story Time Adventure",
-            "description": "In this application that I created, in collaboration with fellow students, the user is able to create an account and sign in using a password that is encrypted and stored in a database. After signing in the user can add children to their account and provide information specific to each child. The user can then create a unique story that incorporates the information for the child.",
-            "repository": "https://github.com/eh4git/StoryTime",
-            "deployed": "https://fathomless-waters-70908.herokuapp.com/",
-            "image": "https://github.com/eh4git/Portfolio_V1/blob/master/public/assets/images/projects/storyTime.PNG?raw=true"
-        },
-        {
-            "name": "Once Upon an Adventure",
-            "description": "This full stack application incorporates MongoDB, Express, React, Node.js. The application give users the ability to create a unique story while playing a game that contains educational mini games. Save the king then challenge him to a game!",
-            "repository": "https://github.com/eh4git/once-upon-an-adventure",
-            "deployed": "https://www.onceuaa.com/",
-            "image": "https://github.com/eh4git/Portfolio_V1/blob/master/public/assets/images/projects/onceUponAnAdventure.png?raw=true"
-        },
-        {
-            "name": "Along With Me",
-            "description": "An application built using React Native that enhances a users experience while reading their favorite literature. Provides the ability for users to trigger sounds that are tied to key words within the text. Key words are highlighted to show users what can be pressed to trigger the corresponding sound.",
-            "repository": "https://github.com/eh4git/Along-With-Me",
-            "deployed": "https://play.google.com/store/apps/details?id=com.rnapp2021",
-            "image": "https://github.com/eh4git/Portfolio_V1/blob/master/public/assets/images/projects/along-with-me.png?raw=true"
-        },
-        {
-            "name": "E-Commerce Application",
-            "description": "A React application that provides a basic framework for an e-commerce web application. Users can favorite and add items to a cart. Users can also manage the items in the cart and can place their order",
-            "repository": "https://github.com/eh4git/Scrimba_ECommerce",
-            "deployed": false,
-            "image": "https://github.com/eh4git/Portfolio_V1/blob/master/public/assets/images/projects/scrimbaEcommerce.PNG?raw=true"
-        },
-        {
-            "name": "Time2Fly",
-            "description": "A React Native application that allows flight attendants to easily track their hours and know when they will reach their maximum allowed work time. ",
-            "repository": "Private",
-            "deployed": false,
-            "image": "https://github.com/eh4git/Portfolio_V1/blob/master/public/assets/images/projects/time2Fly.png?raw=true"
-        },
-        {
-            "name": "React TypeScript Minesweeper",
-            "description": "The classic game of minesweeper with a few improvements. Users always open multiple tiles on their first click and can choose four different difficulty settings. More features and UI improvements to come.",
-            "repository": "https://github.com/eh4git/Scrimba_ECommerce",
-            "deployed": false,
-            "image": "https://github.com/eh4git/Portfolio_V1/blob/master/public/assets/images/projects/reactMinesweeper.PNG?raw=true"
-        },
-
-    ],
-    "resume":
+  repositories: [
     {
-        "pdf": "/docs/WebDevResume.pdf",
-        "word": "/docs/WebDevResume.docx"
+      name: "YouMdB Movie Application",
+      href: "https://github.com/abazrafshan/YouMDb-Movie-App.git",
     },
-    "name": "Erik Hirsch",
-    "email": "ehirsch760@gmail.com",
-    "phone": "760 420-4308",
-    "location": "Apple Valley, CA 92308",
+    {
+      name: "Weather Forecast Application",
+      href: "https://github.com/eh4git/WeatherApp",
+    },
+    {
+      name: "Coding Quiz Challenge Application",
+      href: "https://github.com/eh4git/CodingQuiz",
+    },
+  ],
 
-    "landingPage": "/",
-    "profilePage": "/profile",
-    "profilePicture": "https://github.com/eh4git/Portfolio_V1/blob/master/public/assets/images/ProfilePic.jpg?raw=true",
-    "skills": {
-        language: ['HTML', 'CSS', 'JavaScript', 'PHP', 'TypeScript'],
-        technical: [ 'jQuery', 'AJAX', 'Express', 'Node.js', 'React.js', 'React Native', 'Redux', 'Bootstrap', 'MaterialUI', 'Git', 'Apache'],
-        database :['MySQL', 'MongoDB', 'Firebase'],
-        os: ["Windows", "Mac", "Linux: Debian10, Ubuntu"],
-        other: ["Effective communication", "Team Player", "Leadership", "Reliable", "Self-motivated", "Adaptable"]
-    }
-}
+  profiles: [
+    {
+      name: "LinkedIn",
+      href: "https://www.linkedin.com/in/e-hirsch/",
+    },
+    {
+      name: "Github",
+      href: "https://github.com/eh4git",
+    },
+  ],
+
+  profileSections: [
+    {
+      name: "Profile",
+      href: "/profile/",
+    },
+    {
+      name: "Projects",
+      href: "/projects/",
+    },
+    {
+      name: "Contact",
+      href: "/contact/",
+    },
+  ],
+
+  projects: [
+    {
+      name: "YouMdB Movie Application",
+      description:
+        "This application was built to provide internet users with a centralized location to look up information on movies and to be presented with movie trailers and similar movie recommendations. This project was a great exercise in utilizing multiple entertainment API's to present the user with useful information relevant to the scope of the application. Additionally, building this application helped reinforce skills with jquery, HTML, and CSS.",
+      repository: "https://github.com/eh4git/YouMDb-Movie-App",
+      deployed: "https://eh4git.github.io/YouMDb-Movie-App/",
+      image: "https://d3kccomi9ge1kz.cloudfront.net/YouMBdApp.png",
+    },
+    {
+      name: "Weather Forecast Application",
+      description:
+        "In this application I have created a weather dashboard. The dashboard contains a search box that the user will input their desired city name, when the search button is clicked the weather for the city will be displayed as well as a five day forecast below it.",
+      repository: "https://github.com/eh4git/WeatherApp",
+      deployed: "https://eh4git.github.io/WeatherApp/",
+      image: "https://d3kccomi9ge1kz.cloudfront.net/WeatherDashboard4.png",
+    },
+    {
+      name: "Story Time Adventure",
+      description:
+        "In this application that I created, in collaboration with fellow students, the user is able to create an account and sign in using a password that is encrypted and stored in a database. After signing in the user can add children to their account and provide information specific to each child. The user can then create a unique story that incorporates the information for the child.",
+      repository: "https://github.com/eh4git/StoryTime",
+      deployed: "https://fathomless-waters-70908.herokuapp.com/",
+      image: "https://d3kccomi9ge1kz.cloudfront.net/storyTime.png",
+    },
+    {
+      name: "Once Upon an Adventure",
+      description:
+        "This full stack application incorporates MongoDB, Express, React, Node.js. The application give users the ability to create a unique story while playing a game that contains educational mini games. Save the king then challenge him to a game!",
+      repository: "https://github.com/eh4git/once-upon-an-adventure",
+      deployed: "https://www.onceuaa.com/",
+      image: "https://d3kccomi9ge1kz.cloudfront.net/onceUponAnAdventure.png",
+    },
+    {
+      name: "Along With Me",
+      description:
+        "An application built using React Native that enhances a users experience while reading their favorite literature. Provides the ability for users to trigger sounds that are tied to key words within the text. Key words are highlighted to show users what can be pressed to trigger the corresponding sound.",
+      repository: "https://github.com/eh4git/Along-With-Me",
+      deployed: "https://play.google.com/store/apps/details?id=com.rnapp2021",
+      image: "https://d3kccomi9ge1kz.cloudfront.net/along-with-me-logo.png",
+    },
+    {
+      name: "E-Commerce Application",
+      description:
+        "A React application that provides a basic framework for an e-commerce web application. Users can favorite and add items to a cart. Users can also manage the items in the cart and can place their order",
+      repository: "https://github.com/eh4git/Scrimba_ECommerce",
+      deployed: false,
+      image: "https://d3kccomi9ge1kz.cloudfront.net/Ecommerce.png",
+    },
+    {
+      name: "Time2Fly",
+      description:
+        "A React Native application that allows flight attendants to easily track their hours and know when they will reach their maximum allowed work time. ",
+      repository: "Private",
+      deployed: false,
+      image: "https://d3kccomi9ge1kz.cloudfront.net/image-coming-soon.jpg",
+    },
+    {
+      name: "React TypeScript Minesweeper",
+      description:
+        "The classic game of minesweeper with a few improvements. Users always open multiple tiles on their first click and can choose four different difficulty settings. More features and UI improvements to come.",
+      repository: "https://github.com/eh4git/Scrimba_ECommerce",
+      deployed: false,
+      image: "https://d3kccomi9ge1kz.cloudfront.net/image-coming-soon.jpg",
+    },
+  ],
+  resume: {
+    pdf: "/docs/WebDevResume.pdf",
+    word: "/docs/WebDevResume.docx",
+  },
+  name: "Erik Hirsch",
+  email: "ehirsch760@gmail.com",
+  phone: "760 420-4308",
+  location: "Apple Valley, CA 92308",
+
+  landingPage: "/",
+  profilePage: "/profile",
+  profilePicture:
+    "https://d3kccomi9ge1kz.cloudfront.net/Optimized-ProfilePic.jpg",
+  skills: {
+    language: ["HTML", "CSS", "JavaScript", "PHP", "TypeScript"],
+    technical: [
+      "jQuery",
+      "AJAX",
+      "Express",
+      "Node.js",
+      "React.js",
+      "React Native",
+      "Redux",
+      "Bootstrap",
+      "MaterialUI",
+      "Git",
+      "Apache",
+    ],
+    database: ["MySQL", "MongoDB", "Firebase"],
+    os: ["Windows", "Mac", "Linux: Debian10, Ubuntu"],
+    other: [
+      "Effective communication",
+      "Team Player",
+      "Leadership",
+      "Reliable",
+      "Self-motivated",
+      "Adaptable",
+    ],
+  },
+};
 
 export default profileText;
